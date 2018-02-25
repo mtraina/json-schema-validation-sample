d706d6c77e6b0cbab4346d3031bc288a4a35e6f7
mtraina <matteo.traina.mail@gmail.com>
2018-02-25 18:47:30 +0100

    controller added

--- src/main/kotlin/com/mtraina/jsonschemavalidationsample/JsonSchemaValidationSampleApplication.kt
+++ src/main/kotlin/com/mtraina/jsonschemavalidationsample/JsonSchemaValidationSampleApplication.kt
@@ -2,6 +2,9 @@ package com.mtraina.jsonschemavalidationsample
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class JsonSchemaValidationSampleApplication
@@ -9,3 +12,18 @@ class JsonSchemaValidationSampleApplication
 fun main(args: Array<String>) {
     runApplication<JsonSchemaValidationSampleApplication>(*args)
 }
+
+data class BookDetail(val dimensions: String, val language: String)
+data class Book(val isbn10: String, val title: String, val details: BookDetail)
+
+val books = setOf(Book("0000000000", "count of montecristo", BookDetail("5.2 x 2.2 x 7.8 inches", "english")),
+                            Book("1111111111", "ulysses", BookDetail("7 x 0.9 x 10 inches", "english")))
+
+@RestController
+@RequestMapping("/books")
+class BookController {
+
+    @GetMapping
+    fun getBooks() = books
+}
+
